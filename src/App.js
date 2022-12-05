9ec46aac57685e2d16a61ed6ccbadaa763cad0e1
phuc131102 <115385594+phuc131102@users.noreply.github.com>
2022-12-05 23:15:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,36 @@ function App() {
         case "/message":
             Component = Message
             break
+        case "/cnvs":
+            Component = FirstTab
+            break
+        case "/cncr":
+            Component = SecondTab
+            break
+        case "/cnvs01":
+            Component = Info01
+            break
+        case "/cnvs02":
+            Component = Info02
+            break
+        case "/cnvs03":
+            Component = Info03
+            break
+        case "/cnvs04":
+            Component = Info04
+            break
+        case "/cncr01":
+            Component = Info11
+            break
+        case "/cncr02":
+            Component = Info12
+            break
+        case "/cncr03":
+            Component = Info13
+            break
+        case "/cncr04":
+            Component = Info14
+            break
         default:
     }
   
